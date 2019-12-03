903fcd699cffa7c3e1a3452027974fb4009b2f74
d7p4n4 <bercikezs199999999@gmail.com>
2019-12-03 10:13:27 +0100

    Add project files.

---
+++ CSAc4yJSONObject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSAc4yJSONObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSSerializerLib.Serialize _serialize = new CSSerializerLib.Serialize();
+
+            _serialize.SerializeMethod("d:\\Server\\Visual_studio\\output_Xmls\\", typeof(Program));
+        }
+    }
+}
